736cb75cf4999002821cc6617447980047858fc9
Tray Lewin <tray.lewin@live.com>
2021-11-29 16:12:42 -0800

    add chain url

--- contracts/ore-token/contracts/eosio.token/deploy.sh
+++ contracts/ore-token/contracts/eosio.token/deploy.sh
@@ -1,1 +1,2 @@
-cleos set contract eosio.token ./build -p eosio.token -p system.ore
+# cleos -u https://ore.openrights.exchange:443 set contract eosio.token ./build eosio.token.wasm eosio.token.abi -p eosio.token -p system.ore
+cleos -u https://ore-staging.openrights.exchange:443 set contract eosio.token ./build eosio.token.wasm eosio.token.abi -p eosio.token -p system.ore
