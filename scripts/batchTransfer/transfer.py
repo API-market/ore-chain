63cc1d7494a1eda807b14721fa6a57fe92691bac
Tray Lewin <tray.lewin@live.com>
2022-03-21 16:16:12 -0700

    add memo to batchTransfer

--- scripts/batchTransfer/transfer.py
+++ scripts/batchTransfer/transfer.py
@@ -12,7 +12,7 @@ outputFileName = 'transfer-output.txt'
 for line in Lines:
   print(line)
   jsoned = json.loads(line)
-  command = "cleos -u " + sys.argv[1] + " transfer " + jsoned["fromAccount"] +  " " +  jsoned["toAccount"] + " \"" + jsoned["amount"] + "\" \"\" -p " + jsoned["fromAccount"]
+  command = "cleos -u " + sys.argv[1] + " transfer " + jsoned["fromAccount"] +  " " +  jsoned["toAccount"] + " \"" + jsoned["amount"] + "\"" + " \"" + jsoned["memo"] + "\" -p " + jsoned["fromAccount"]
   print(command)
   with open(outputFileName, 'a') as the_file:
     the_file.write('\n' + command + '\n')
