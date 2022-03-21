23ed460a4203b5f76270d1294e1411302525d973
Tray Lewin <tray.lewin@live.com>
2022-03-21 16:17:14 -0700

    add public keys to json

--- scripts/batchCreateAccounts/createAccounts.py
+++ scripts/batchCreateAccounts/createAccounts.py
@@ -11,7 +11,7 @@ outputFileName = 'createAccounts-output.txt'
 for line in Lines:
   print(line)
   jsoned = json.loads(line)
-  command = "cleos -u " + sys.argv[1] + " system newaccount " + jsoned["creator"] +  " " +  jsoned["createAccount"] + " \"" + sys.argv[2] + "\" \"" + sys.argv[3] + "\" --stake-net \"0.0100 SYS\" --stake-cpu \"0.0100 SYS\" --buy-ram-kbytes 4 -p " + jsoned["creator"]
+  command = "cleos -u " + sys.argv[1] + " system newaccount " + jsoned["creator"] +  " " +  jsoned["createAccount"] + " \"" + jsoned["ownerPublicKey"] + "\" \"" + jsoned["activePublicKey"] + "\" --stake-net \"0.0100 SYS\" --stake-cpu \"0.0100 SYS\" --buy-ram-kbytes 4 -p " + jsoned["creator"]
   print(command)
   with open(outputFileName, 'a') as the_file:
     the_file.write('\n' + command + '\n')
